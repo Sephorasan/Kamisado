c4eb5859b1256461d37d063a7af98a07046aa0f1
Sephora <karkarsephora@gmail.com>
2020-02-06 15:45:08 +0100

    methode liste des pions

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -2,11 +2,19 @@ package Model;
 
 import java.util.ArrayList;
 
-public class Joueur {/*
+public class Joueur{
+    private int NbrPions = 16;
     private ArrayList<Pions> ListeDesPions;
+
+    public Joueur(){
+        this.ListeDesPions = ArrayList<>();
+        for (int i = 0; i < NbrPions; i++ ){
+            ListeDesPions.add(new Pion());
+        }
+    }
 
     public void deplacerPion(){
 
-    }*/
+    }
 }
 
