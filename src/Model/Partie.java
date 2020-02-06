39ece2e98516ca67e71d14d1b2cf3ded073ccba2
Sephora <karkarsephora@gmail.com>
2020-02-06 16:08:50 +0100

    creation plateau

--- src/Model/Partie.java
+++ src/Model/Partie.java
@@ -7,6 +7,26 @@ public class Partie {
     private String DerniereCouleur;
     private String DernierePositionCouleur;
     public static final int COTE = 8;
+    public Case[][] cases;
+    public enum Valeur { ORANGE, PINK, BLUE, PURPLE, YELLOW, RED, GREEN, BROWN }
+
+    private static final Valeur[][]  plateau = {
+            { Valeur.ORANGE, Valeur.RED, Valeur.GREEN, Valeur.PINK,Valeur.YELLOW, Valeur.BLUE,Valeur.PURPLE,
+                    Valeur.BROWN },
+            {Valeur.BLUE,Valeur.ORANGE, Valeur.PINK,Valeur.PURPLE, Valeur.RED, Valeur.YELLOW,Valeur.BROWN,
+                    Valeur.GREEN },
+            { Valeur.PURPLE, Valeur.PINK,Valeur.ORANGE, Valeur.BLUE, Valeur.GREEN, Valeur.BROWN,Valeur.YELLOW,
+                    Valeur.RED },
+            { Valeur.PINK, Valeur.GREEN, Valeur.RED, Valeur.ORANGE, Valeur.BROWN, Valeur.PURPLE,Valeur.BLUE,
+                    Valeur.YELLOW },
+            {Valeur.YELLOW, Valeur.BLUE,Valeur.PURPLE,Valeur.BROWN,Valeur.ORANGE,Valeur.RED, Valeur.GREEN,
+                    Valeur.PINK },
+            {Valeur.RED, Valeur.YELLOW,Valeur.BROWN,Valeur.GREEN, Valeur.BLUE,Valeur.ORANGE,Valeur.PINK,
+                    Valeur.PURPLE },
+            {Valeur.GREEN,Valeur.BROWN, Valeur.YELLOW,Valeur.RED,Valeur.PURPLE, Valeur.PINK, Valeur.ORANGE,
+                    Valeur.BLUE },
+            {Valeur.BROWN,Valeur.PURPLE,Valeur.BLUE,Valeur.YELLOW, Valeur.PINK, Valeur.GREEN, Valeur.RED,
+                    Valeur.ORANGE } };
 
 
     public Partie(String derniereCouleur, String dernierePositionCouleur) {
@@ -14,14 +34,17 @@ public class Partie {
         DernierePositionCouleur = dernierePositionCouleur;
     }
 
-/*    public void createPlateau() {
-        String[][] partie = new String[COTE][COTE];
-        for (int i = 0; i < Partie[].length; i++) {
-            for (int j = 0; j < Partie[i].length; j++) {
+    public void createPlateau() {
+
+        for(int i=0; i<plateau.length; i++) {
+            for(int j=0; j<plateau[i].length; j++) {
+                plateau =
+                // ici faut travailler
 
             }
         }
-    }*/
+    }
+
 
     public String getDerniereCouleur() {
         return DerniereCouleur;
--- src/View/ViewJeu.java
+++ src/View/ViewJeu.java
@@ -1,8 +1,33 @@
 package View;
 
+import javafx.scene.image.Image;
+
 public class ViewJeu {
+
+    private Image ORANGE;
+    private Image BLUE;
+    private Image PURPLE;
+    private Image PINK;
+    private Image YELLOW;
+    private Image RED;
+    private Image GREEN;
+    private Image BROWN;
+
 
     public void majPositionPion(){
 
     }
+
+    public void ChargeImage() {
+        ORANGE = new Image("Asset/Images/ORANGE.png");
+        BLUE = new Image("Asset/Images/BLUE.png");
+        PURPLE = new Image("Asset/Images/PURPLE.png");
+        PINK = new Image("Asset/Images/PINK.png");
+        YELLOW = new Image("Asset/Images/YELLOW.png");
+        RED = new Image("Asset/Images/RED.png");
+        GREEN = new Image("Asset/Images/GREEN.png");
+        BROWN = new Image("Asset/Images/BROWN.png");
+    }
+
+
 }
