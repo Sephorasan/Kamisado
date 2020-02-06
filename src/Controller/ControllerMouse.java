e10df3f987ae70c8c8606b6b8581aceeafbd3e9a
Sephora <karkarsephora@gmail.com>
2020-02-06 10:59:07 +0100

    ajout du code de sarah sur controller mouse

--- src/Controller/ControllerMouse.java
+++ src/Controller/ControllerMouse.java
@@ -1,4 +1,24 @@
 package Controller;
 
-public class ControllerMouse {
+import Model.Jeu;
+import View.ViewHandler;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+
+public class ControllerMouse implements EventHandler<MouseEvent> {
+    private ViewHandler launcher;
+    private Jeu model;
+
+    public ControllerMouse(ViewHandler launcher, Jeu model) {
+        this.model = model;
+        this.launcher = launcher;
+        //this.launcher.setEnventHandlerInGame(this);
+    }
+
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+        System.out.println(mouseEvent.getSource());
+        //if(mouseEvent.getSource().equals(launcher.getViewInGame())){}
+    }
 }
